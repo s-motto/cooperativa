ec73cbe020fb3178f2dacb0de71245ddb3a1bb57
s-motto <brinamotto@gmail.com>
2026-04-13 18:03:42 +0200

    feat: add Tailwind CSS script to app and guest layouts

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,6 +11,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.tailwindcss.com"></script>
         <!-- Scripts -->
         {{--@vite(['resources/css/app.css', 'resources/js/app.js'])--}}
     </head>
